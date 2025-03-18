2c179251bcb692f0addf6ba1bca520405eb65f54
Nhat Le <nhatleminh1997@gmail.com>
2025-03-18 15:46:24 -0400

    added install scripts and udev rules

--- install.sh
+++ install.sh
@@ -1,6 +1,53 @@
-cd ros2_ws/src/ds4drv
+# Assume this repo is already cloned to $HOME directory
+
+### Remove a conflicting package
+sudo apt remove brltty
+
+### Install ZED SDK
+cd $HOME
+wget --content-disposition https://download.stereolabs.com/zedsdk/4.2/l4t36.4/jetsons
+chmod +x ZED_SDK_Tegra_L4T36.4_v4.2.5.zstd.run
+./ZED_SDK_Tegra_L4T36.4_v4.2.5.zstd.run -- silent
+
+### Install ROS 2
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install ros-humble-desktop
+sudo apt install ros-dev-tools
+source /opt/ros/humble/setup.bash
+echo "source /opt/ros/humble/setup.bash" >> $HOME/.bashrc
+
+# Install dependencies
+sudo apt-get install libqt5serialport5-dev # for witmotion IMU
+sudo apt-get install build-essential git cmake libasio-dev can-utils # for Scout ugv_sdk
+
+
+# Copy udev rules 
+cd $HOME/socnav_data_collection
+sudo cp -r etc/udev/rules.d/* /etc/udev/rules.d/
+
+# For PS4 controller package
+cd $HOME/socnav_data_collection/ros2_ws/src/ds4drv
 mkdir -p ~/.local/lib/python3.10/site-packages
 python3 setup.py install --prefix ~/.local
-sudo cp udev/50-ds4drv.rules /etc/udev/rules.d/
+# sudo cp udev/50-ds4drv.rules /etc/udev/rules.d/ # already did this
 sudo udevadm control --reload-rules
 sudo udevadm trigger
