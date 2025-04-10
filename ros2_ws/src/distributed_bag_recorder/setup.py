ac7efe967034a8636685363890b881ae89d5effd
daeunSong <daeun7250@gmail.com>
2025-04-10 14:10:10 -0400

    updated distributed bag recorder

--- ros2_ws/src/distributed_bag_recorder/setup.py
+++ ros2_ws/src/distributed_bag_recorder/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'distributed_bag_recorder'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
@@ -13,8 +13,8 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='your_email@example.com',
+    maintainer='Nhat Le',
+    maintainer_email='nhatleminh1997@gmail.com',
     description='ROS 2 package to control distributed bag recording across a laptop and robot',
     license='Apache License 2.0',
     tests_require=['pytest'],
